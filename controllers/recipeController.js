1ac7002a24c44e0d0bdb17eb33e3ad351d6d1f7e
susmitajadhav <45027764+susmitajadhav@users.noreply.github.com>
2024-08-28 12:26:14 +0530

    Update recipeController.js

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,39 +1,37 @@
-//recipeController.js
-const Recipe = require('../models/recipe')
+const Recipe = require('../models/recipe');
 
 exports.getRecipe = async (req, res) => {
     try {
-        const data = await Recipe.find()
-        return res.json({ errorsRecipe: false, data: data })
+        const data = await Recipe.find();
+        return res.json({ errorsRecipe: false, data: data });
     } catch (error) {
-        return res.status(400).json({ errors: true, message: error.message })
+        return res.status(400).json({ errors: true, message: error.message });
     }
-}
+};
 
 exports.postRecipe = async (req, res) => {
     try {
-        const data = await Recipe.create(req.body)
-        return res.json({ errors: false, data: data })
+        const data = await Recipe.create(req.body);
+        return res.json({ errors: false, data: data });
     } catch (error) {
-        return res.status(400).json({ errors: true, message: error.message })
+        return res.status(400).json({ errors: true, message: error.message });
     }
-}
+};
 
 exports.putRecipe = async (req, res) => {
     try {
-        const data = await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        return res.json({ errors: false, data: data })
+        const data = await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        return res.json({ errors: false, data: data });
     } catch (error) {
-        return res.status(400).json({ errors: true, message: error.message })
+        return res.status(400).json({ errors: true, message: error.message });
     }
-}
+};
 
 exports.deleteRecipe = async (req, res) => {
-
     try {
-        const data = await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        return res.json({ errors: false, data: data })
+        const data = await Recipe.findByIdAndDelete(req.params.id);
+        return res.json({ errors: false, data: data });
     } catch (error) {
-        return res.status(400).json({ errors: true, message: error.message })
+        return res.status(400).json({ errors: true, message: error.message });
     }
-}
+};
